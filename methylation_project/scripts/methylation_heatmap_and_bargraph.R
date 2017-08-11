3315e02ee783baae3d90ed767de9e15843b1ff59
nagreme <nadpulgarvidal@gmail.com>
2017-08-11 17:40:29 -0500

    Start trans. elem. work, change barchart colours

--- methylation_project/scripts/methylation_heatmap_and_bargraph.R
+++ methylation_project/scripts/methylation_heatmap_and_bargraph.R
@@ -3,6 +3,7 @@
 # Nadège Pulgar-Vidal
 # 
 # Methylation Heatmap and Bar Graph
+# And transposable element density!
 # --------------------------------------------------------------
 
 # ============================
@@ -13,31 +14,42 @@ library(magrittr) # for piping (%>%)
 library(ggplot2) # for nicer plotting functionality
 library(RColorBrewer) # for ready made colour palettes
 library(dplyr) # for nice table manipulation
-library(gplots) # for heatmap
 
 
 # ============================
-# --- SETUP/INPUT
+# --- SETUP
 # ============================
-
-# List of files
-files <- c("/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data/methylation_calls_bsmap/GLOB_meth.txt",
-          "/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data/methylation_calls_bsmap/MG_meth.txt")
-
-# List of samples (matching files)
-sample_tags <- c("GLOB",
-                "MG")
-
-# Name and location of output heatmap
-outfile_path <- "/home/mark/Documents/Nadege/belmonte_lab/methylation_project/visualizations/output_plot.pdf"
 
 # Bin size (absolute positions)
 bin_size <- 100000
+
+# Name and location of output file
+outfile_path <- "/home/mark/Documents/Nadege/belmonte_lab/methylation_project/visualizations/output_plot.pdf"
+
+
+# *** Methlyation Data ***
+
+# List of input files
+files <- c("/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data/methylation_calls_bsmap/GLOB_meth.txt",
+          "/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data/methylation_calls_bsmap/MG_meth.txt")
+
+# List of sample names (matching files)
+sample_tags <- c("GLOB",
+                "MG")
+
+
+# *** Transposable Elements ***
+
+# Transposable elements file
+trans_elem_file <- "/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data/DH12075_trans_elem.gff"
+
 
 
 # ============================
 # --- READ/FORMAT DATA
 # ============================
+
+# *** Methlyation Data ***
 
 data_list <- list()
 
@@ -81,42 +93,72 @@ data_binned_full$sample_f = factor(data_binned_full$sample, levels = c("MG", "GL
 
 # TODO: Can I write ^this^ as a mutate() call?
 
+# For reordering the chr names
+data_binned_full$chr_order <- factor(data_binned_full$chr, levels = c("N1", "N2", "N3", "N4", "N5", "N6", "N7", "N8", "N9", "N10",
+                                                                      "N11", "N12", "N13", "N14", "N15", "N16", "N17", "N18", "N19"))
 
-# --- Separate data into 2 sets by chromosome (N1-N9, N10-N19)
-data_n1_9 <- filter(data_binned_full, chr %in% c("N1", "N2", "N3", "N4", "N5", "N6", "N7", "N8", "N9"))
-data_n10_19 <- filter(data_binned_full, chr %in% c("N10", "N11", "N12", "N13", "N14", "N15", "N16", "N17", "N18", "N19"))
+# Separate data into 2 sets by chromosome (N1-N9, N10-N19)
+data_n1_10 <- filter(data_binned_full, chr %in% c("N1", "N2", "N3", "N4", "N5", "N6", "N7", "N8", "N9", "N10"))
+data_n11_19 <- filter(data_binned_full, chr %in% c("N11", "N12", "N13", "N14", "N15", "N16", "N17", "N18", "N19"))
 
-# --- Separate CHH context so we can see it better
+# Separate CHH context so we can see it better
 data_CHH <- filter(data_binned_full, context %in% c("CHH"))
+
+
+# *** Transposable Elements ***
+
+# Note: I don't actually know what all the columns but I named them anyways
+
+read.table(file, header=F, sep="\t",
+           col.names = c("chr", "src", "just", "start" ,"end", "score", "strand", 
+                         "myst", "target", "motif", "mstart", "mend")) %>%
+  select(chr, start, end) -> #we only need chr and start for plotting purposes but I'm keeping end just in case for now 
+  data_trans_elem
+  
+
 
 # ============================
 # --- VISUALIZE
 # ============================
 
+# *** Methylation Data ***
+
 # Choose which set of data to plot
 plot_data <- data_binned_full
-plot_data <- data_n1_9
-plot_data <- data_n10_19
+plot_data <- data_n1_10
+plot_data <- data_n11_19
 plot_data <- data_CHH
 
 
 # Bar Chart of average bin ratios
 graph <- ggplot(plot_data, aes(bin, avg_ratio)) +
-  facet_grid(context + sample ~ chr, scales = "free_x") +
-  geom_bar(stat = "identity") +
+  facet_grid(context + sample ~ chr_order, scales = "free_x") +
+  geom_bar(stat = "identity", aes(fill = avg_ratio, colour = avg_ratio)) +
+  guides(fill=FALSE) + 
   labs(title = "Methylation ratio",
        x = paste0("Bin number\n(",bin_size,"bp bins)"),
-       y = "Average bin ratio")
+       y = "Average bin ratio") +
+  scale_color_gradient2(midpoint = 0.5, 
+                        low="#021637", 
+                        mid="#00517C", 
+                        high="#C5F6D5") #sad snape
+                                                      
+
+ggsave(outfile_path, height = 7, width = 12) #use width 20ish for full
+
+
+sad_snape_colours <- colorRampPalette(c("#021637","#00517C", "#C5F6D5"))(100)
 
 # Heatmap of average bin methylation ratio
-graph <- ggplot(plot_data, aes(x = bin, y = sample_f, fill = avg_ratio)) +
-  facet_grid(context ~ chr, switch = "y", scales = "free_x") +
-  geom_tile(aes(colour = avg_ratio)) + 
+graph <- ggplot(plot_data, aes(x = bin, y = sample_f) +
+  facet_grid(context ~ chr_order, switch = "y", scales = "free_x") +
+  geom_tile(aes(fill = avg_ratio), colour = avg_ratio)) + 
   # the colour parameter above colours the tile outlines same as fill
   labs(title = "Methylation ratio",
        x = paste0("Bin number\n(",bin_size,"bp bins)"),
-       y = "")  +
-  #scale_fill_distiller(palette = "Spectral")
+       y = "") #+
+# scale_fill_manual(values = sad_snape_colours)
+  # scale_fill_distiller(palette = "Spectral")
 
 # TODO: Pick colour palette ^^^
 
@@ -124,6 +166,9 @@ graph <- ggplot(plot_data, aes(x = bin, y = sample_f, fill = avg_ratio)) +
        
 ggsave(outfile_path, height = 7, width = 12)
 
+
+
+# *** Transposable Elements ***
 
 
 
