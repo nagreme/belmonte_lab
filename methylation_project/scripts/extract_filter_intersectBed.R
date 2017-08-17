a12cbae0b8d74912dcdace05288920aed0c63ce1
nagreme <nadpulgarvidal@gmail.com>
2017-08-17 16:05:20 -0500

    Add processing of gene expression files

--- methylation_project/scripts/extract_filter_intersectBed.R
+++ methylation_project/scripts/extract_filter_intersectBed.R
@@ -9,6 +9,7 @@
 #
 # Ran sed -i.orig -r "s/.*ID=(Bna.*);.*$/\1/" on all output 
 # files to get simple gene list
+# sed -i.orig -r "s/.*(Bna.*)\t.*\t.*$/\1/" * (for the second set)
 #
 # This was used to remove the first line of each file: (run in output dir)
 # for FILE in *; do tail -n +2 "$FILE" > "$FILE.tmp" && mv "$FILE.tmp" "$FILE"; done
@@ -28,29 +29,51 @@ library(magrittr)
 # ==========================
 
 # --- Input files 
-gene_CpG_file <- '/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data/bedtools_intersects/genes_CpG_intersect.txt'
-gene_CHG_file <- '/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data/bedtools_intersects/genes_CHG_intersect.txt'
-gene_CHH_file <- '/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data/bedtools_intersects/genes_CHH_intersect.txt'
-flank_CpG_file <- '/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data/bedtools_intersects/gene_flanks_CpG_intersect.txt'
-flank_CHG_file <- '/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data/bedtools_intersects/gene_flanks_CHG_intersect.txt'
-flank_CHH_file <- '/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data/bedtools_intersects/gene_flanks_CHH_intersect.txt'
+# gene_CpG_file <- '/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data/bedtools_intersects/genes_CpG_intersect.txt'
+# gene_CHG_file <- '/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data/bedtools_intersects/genes_CHG_intersect.txt'
+# gene_CHH_file <- '/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data/bedtools_intersects/genes_CHH_intersect.txt'
+# flank_CpG_file <- '/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data/bedtools_intersects/gene_flanks_CpG_intersect.txt'
+# flank_CHG_file <- '/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data/bedtools_intersects/gene_flanks_CHG_intersect.txt'
+# flank_CHH_file <- '/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data/bedtools_intersects/gene_flanks_CHH_intersect.txt'
+
+gene_CpG_file <- '/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data/bedtools_intersects/differential_expression/GLOB_vs_MG_genes_CpG_intersect.txt'
+gene_CHG_file <- '/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data/bedtools_intersects/differential_expression/GLOB_vs_MG_genes_CHG_intersect.txt'
+gene_CHH_file <- '/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data/bedtools_intersects/differential_expression/GLOB_vs_MG_genes_CHH_intersect.txt'
+flank_CpG_file <- '/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data/bedtools_intersects/differential_expression/GLOB_vs_MG_flanks_CpG_intersect.txt'
+flank_CHG_file <- '/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data/bedtools_intersects/differential_expression/GLOB_vs_MG_flanks_CHG_intersect.txt'
+flank_CHH_file <- '/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data/bedtools_intersects/differential_expression/GLOB_vs_MG_flanks_CHH_intersect.txt'
+
 
 in_files <- list(gene_CpG_file, gene_CHG_file, gene_CHH_file, 
                  flank_CpG_file, flank_CHG_file, flank_CHH_file)
 
+
 # --- Output files
-gene_CpG_hypo_file <- '/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data/methylated_lists/genes_CpG_hypo.txt'
-gene_CpG_hyper_file <- '/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data/methylated_lists/genes_CpG_hyper.txt'
-gene_CHG_hypo_file <- '/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data/methylated_lists/genes_CHG_hypo.txt'
-gene_CHG_hyper_file <- '/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data/methylated_lists/genes_CHG_hyper.txt'
-gene_CHH_hypo_file <- '/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data/methylated_lists/genes_CHH_hypo.txt'
-gene_CHH_hyper_file <- '/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data/methylated_lists/genes_CHH_hyper.txt'
-flank_CpG_hypo_file <- '/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data/methylated_lists/flanks_CpG_hypo.txt'
-flank_CpG_hyper_file <- '/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data/methylated_lists/flanks_CpG_hyper.txt'
-flank_CHG_hypo_file <- '/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data/methylated_lists/flanks_CHG_hypo.txt'
-flank_CHG_hyper_file <- '/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data/methylated_lists/flanks_CHG_hyper.txt'
-flank_CHH_hypo_file <- '/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data/methylated_lists/flanks_CHH_hypo.txt'
-flank_CHH_hyper_file <- '/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data/methylated_lists/flanks_CHH_hyper.txt'
+# gene_CpG_hypo_file <- '/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data/methylated_lists/genes_CpG_hypo.txt'
+# gene_CpG_hyper_file <- '/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data/methylated_lists/genes_CpG_hyper.txt'
+# gene_CHG_hypo_file <- '/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data/methylated_lists/genes_CHG_hypo.txt'
+# gene_CHG_hyper_file <- '/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data/methylated_lists/genes_CHG_hyper.txt'
+# gene_CHH_hypo_file <- '/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data/methylated_lists/genes_CHH_hypo.txt'
+# gene_CHH_hyper_file <- '/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data/methylated_lists/genes_CHH_hyper.txt'
+# flank_CpG_hypo_file <- '/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data/methylated_lists/flanks_CpG_hypo.txt'
+# flank_CpG_hyper_file <- '/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data/methylated_lists/flanks_CpG_hyper.txt'
+# flank_CHG_hypo_file <- '/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data/methylated_lists/flanks_CHG_hypo.txt'
+# flank_CHG_hyper_file <- '/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data/methylated_lists/flanks_CHG_hyper.txt'
+# flank_CHH_hypo_file <- '/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data/methylated_lists/flanks_CHH_hypo.txt'
+# flank_CHH_hyper_file <- '/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data/methylated_lists/flanks_CHH_hyper.txt'
+
+gene_CpG_hypo_file <- '/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data/bedtools_intersects/differential_expression/methylated_lists/GLOB_vs_MG_genes_CpG_hypo.txt'
+gene_CpG_hyper_file <- '/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data/bedtools_intersects/differential_expression/methylated_lists/GLOB_vs_MG_genes_CpG_hyper.txt'
+gene_CHG_hypo_file <- '/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data/bedtools_intersects/differential_expression/methylated_lists/GLOB_vs_MG_genes_CHG_hypo.txt'
+gene_CHG_hyper_file <- '/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data/bedtools_intersects/differential_expression/methylated_lists/GLOB_vs_MG_genes_CHG_hyper.txt'
+gene_CHH_hypo_file <- '/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data/bedtools_intersects/differential_expression/methylated_lists/GLOB_vs_MG_genes_CHH_hypo.txt'
+gene_CHH_hyper_file <- '/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data/bedtools_intersects/differential_expression/methylated_lists/GLOB_vs_MG_genes_CHH_hyper.txt'
+flank_CpG_hypo_file <- '/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data/bedtools_intersects/differential_expression/methylated_lists/GLOB_vs_MG_flanks_CpG_hypo.txt'
+flank_CpG_hyper_file <- '/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data/bedtools_intersects/differential_expression/methylated_lists/GLOB_vs_MG_flanks_CpG_hyper.txt'
+flank_CHG_hypo_file <- '/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data/bedtools_intersects/differential_expression/methylated_lists/GLOB_vs_MG_flanks_CHG_hypo.txt'
+flank_CHG_hyper_file <- '/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data/bedtools_intersects/differential_expression/methylated_lists/GLOB_vs_MG_flanks_CHG_hyper.txt'
+flank_CHH_hypo_file <- '/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data/bedtools_intersects/differential_expression/methylated_lists/GLOB_vs_MG_flanks_CHH_hypo.txt'
+flank_CHH_hyper_file <- '/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data/bedtools_intersects/differential_expression/methylated_lists/GLOB_vs_MG_flanks_CHH_hyper.txt'
 
 
 out_files <- list(gene_CpG_hypo_file, gene_CpG_hyper_file,
@@ -59,6 +82,7 @@ out_files <- list(gene_CpG_hypo_file, gene_CpG_hyper_file,
                   flank_CpG_hypo_file, flank_CpG_hyper_file,
                   flank_CHG_hypo_file, flank_CHG_hyper_file,
                   flank_CHH_hypo_file, flank_CHH_hyper_file)
+
 
 # ==========================
 # --- PROCESS 
@@ -70,12 +94,19 @@ for (file in in_files)
 {
    # Read one file
   read.table(file, header=F, sep="\t", 
-             col.names = c("chr.1", "DMR_start", "DMR_end", "meth.diff", 
-                           "chr.2", "src", "feature_type", "feat_start", 
-                           "feat_end", "score", "strand", "myst", "name",
+             # col.names = c("chr.1", "DMR_start", "DMR_end", "meth.diff", 
+             #               "chr.2", "src", "feature_type", "feat_start", 
+             #               "feat_end", "score", "strand", "myst", "name",
+             #               "num_overlap_bp")) %>%
+             col.names = c("chr.1", "DMR_start", "DMR_end", "meth.diff",
+                           "chr.2", "start", "end", "name", "fpkms",
                            "num_overlap_bp")) %>%
-    select(chr.1, name, meth.diff) -> # pull out the pieces we need
+    select(chr.1, name, meth.diff, fpkms) -> # pull out the pieces we need
     dat
+  
+  #Run this if the name column is already simple other than "_flank" tags to remove those
+  dat$name <- unlist(lapply(dat$name, function(x) str_replace(x, "_flank", "")))
+  
   
   # Filter hypomethylated genes and write those out
   dat %>%
