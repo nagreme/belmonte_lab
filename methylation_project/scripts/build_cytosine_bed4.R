da8e181b9c21af269a75c97e265847a92eaef435
nagreme <nadpulgarvidal@gmail.com>
2017-08-23 12:18:59 -0500

    Refine script to separate contexts

--- methylation_project/scripts/build_cytosine_bed4.R
+++ methylation_project/scripts/build_cytosine_bed4.R
@@ -21,8 +21,8 @@ library(magrittr)
 glob_meth_file <- '/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data/methylation_calls_bsmap/GLOB_meth.txt'
 mg_meth_file <- '/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data/methylation_calls_bsmap/MG_meth.txt'
 
-glob_Cs_file <- '/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data/GLOB_meth_Cs.bed'
-mg_Cs_file <- '/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data/MG_meth_Cs.bed'
+glob_Cs_file <- '/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data/GLOB_meth_Cs_'
+mg_Cs_file <- '/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data/MG_meth_Cs_'
 
   
 
@@ -30,21 +30,36 @@ mg_Cs_file <- '/home/mark/Documents/Nadege/belmonte_lab/methylation_project/data
 # --- PROCESS
 # ==========================
 
-infile <- glob_meth_file
-outfile <- glob_Cs_file
+# Selection
+# infile <- glob_meth_file
+# outfile <- glob_Cs_file
 
+infile <- mg_meth_file
+outfile <- mg_Cs_file
+
+# Read in the file, filetring by context and create the needed start and end bed columns
 read_delim(infile, delim ="\t", col_names = T) %>%
   select(chr, pos, context, ratio, eff_CT_count) %>%
   filter(eff_CT_count >= 10) %>%
   mutate(start = pos, end = pos) %>%
-  select(chr, start, end, ratio, eff_CT_count ->
-  dat #for checking
+  select(chr, start, end, ratio, context) ->
+  dat 
 
 
 dat %>%
-  filter(sample == "GLOB") %>%
+  filter(context == "CG") %>%
   select(chr, start, end, ratio) %>%
-  write_delim(outfile, delim="\t", col_names = F)
+  write_delim(paste0(outfile, "CpG.bed"), delim="\t", col_names = F)
+
+dat %>%
+  filter(context == "CHG") %>%
+  select(chr, start, end, ratio) %>%
+  write_delim(paste0(outfile, "CHG.bed"), delim="\t", col_names = F)
+
+dat %>%
+  filter(context == "CHH") %>%
+  select(chr, start, end, ratio) %>%
+  write_delim(paste0(outfile, "CHH.bed"), delim="\t", col_names = F)
 
 
 
