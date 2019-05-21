03994d74849c21579a343cb8e21f5546ecd39e5d
Nadège <pulgarvn@myumanitoba.ca>
2019-05-21 14:30:45 -0500

    Working arg parser for basic args

--- siRNA_project/blastn_print_top_hits.py
+++ siRNA_project/blastn_print_top_hits.py
@@ -2,22 +2,51 @@ import argparse
 from Bio.Blast.Applications import NcbiblastnCommandline
 from Bio.Blast import NCBIXML
 
+# Tutorial/Cookbook
+# http://biopython.org/DIST/docs/tutorial/Tutorial.html#htoc100
+
 # Setup pgm args
 parser = argparse.ArgumentParser()
 
-parser.add_argument('-q', '--query_file', required=True, metavr='query', help='File to blastn')
-parser.add_argument('-p', '--num_threads', default=1, metavar='threads', help="Number of threads to use")
-parser.add_argument('-t', '--taxa_id', metavar='taxaID', help="Filter to inly search whithin specified taxa (NCBI IDs)")
+parser.add_argument('-q', '--query_file', dest='query_file', required=True, metavar='query', help='File to blastn')
+parser.add_argument('-n', '--num_threads', dest='threads', default=1, metavar='threads', help="Number of threads to use")
+parser.add_argument('-t', '--taxa_id', dest='taxa_id', metavar='taxaID', help="Filter to inly search whithin specified taxa (NCBI IDs)")
 
 
 def main():
     args = parser.parse_args()
     # help(NcbiblastnCommandline)
 
-    blastn_cline = NcbiblastnCommandline(db="refseq_rna", query="test_query.fa", window_masker_taxid="3708", num_threads=10, task="blastn", out="test_blastn_results.xml", outfmt=5) # has to be in .xml for the parser
+    query_file = args.query_file
+    threads = args.threads
 
-    print(blastn_cline)
+    print("query file:", query_file)
+    print("threads: ", threads)
 
-    blastn_cline()
+    # you can check if is not None
+    if args.taxa_id is not None:
+        print("taxa id:", args.taxa_id)
 
-    result_handle = open("test_blastn_results.xml")
+    # if args.taxa_id:
+    #     taxa_id = args.taxa_id
+    # else:
+    #     taxa_id = False
+
+
+    # blastn_cline = NcbiblastnCommandline(db="refseq_rna", query="test_query.fa", window_masker_taxid="3708", num_threads=10, task="blastn", out="test_blastn_results.xml", outfmt=5) # has to be in .xml for the parser
+    #
+    # print(blastn_cline)
+    #
+    # blastn_cline()
+    #
+    # result_handle = open("test_blastn_results.xml")
+
+    # parse records
+
+    # print title, e value, and identity for top hit
+
+
+
+
+if __name__ == '__main__':
+    main()
